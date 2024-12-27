a17cd7aab1417942e608f0d859394a12b97c1443
Bastian Bichler <bichlerbastian@gmail.com>
2024-12-27 16:29:48 +0100

    landingPage - theme Provider dont working

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,25 @@ import AboutMe from './page/AboutMe/AboutMe'
 import Docs from './page/Docs/Docs'
 import Tools from './page/Tools/Tools'
 import Ressource from './page/Ressource/Ressource'
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './Theme/Theme'
+import { CssBaseline } from '@mui/material'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Landing />} />
-        <Route path="/docs" element={<Docs />} />
-        <Route path="/tools" element={<Tools />} />
-        <Route path="/aboutme" element={<AboutMe />} />
-        <Route path="/ressources" element={<Ressource />} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/ressources" element={<Ressource />} />
 
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
