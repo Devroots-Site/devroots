849d5b4a9751040a0a77bbe484f25ecdc29441c6
Bichler Bastian <bichlerbastian@gmail.com>
2025-05-11 09:46:33 +0200

    13-npm - updating loadingSpinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,18 +1,20 @@
 import { Box, CircularProgress, Typography } from '@mui/material';
 
 interface ILoadingSpinner {
-  loadingmessage?: string;
+    loadingmessage?: string;
 }
 
 export default function LoadingSpinner(props: ILoadingSpinner) {
-  return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 1 }}>
-      <CircularProgress />
-      {props.loadingmessage && (
-        <Typography variant="body2" color="text.secondary">
-          {props.loadingmessage}
-        </Typography>
-      )}
-    </Box>
-  );
+    return (
+        <Box
+            sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 1, py: 10 }}
+        >
+            <CircularProgress />
+            {props.loadingmessage && (
+                <Typography variant="body2" color="text.secondary">
+                    {props.loadingmessage}
+                </Typography>
+            )}
+        </Box>
+    );
 }
